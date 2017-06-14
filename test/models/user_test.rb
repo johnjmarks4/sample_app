03a2b3653030d7c9578ac559ed68626cab7cda10
Erik Johnson <mindovermiles262@gmail.com>
2017-06-13 19:16:21 -0700

    validate user email using regex

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,6 +6,7 @@ class UserTest < ActiveSupport::TestCase
     @user = User.new(name: "Example User", email: "user@example.com")
   end
 
+# Validate Presence of Name and Email
   test 'should be valid' do
     assert @user.valid?
   end
@@ -18,6 +19,33 @@ class UserTest < ActiveSupport::TestCase
   test 'email should be present' do
     @user.email = "     "
     assert_not @user.valid?
+  end
+
+  test 'name should not be too long' do
+    @user.name = 'a' * 51
+    assert_not @user.valid?
+  end
+
+  test 'email should not be too long' do
+    @user.email = 'a' * 244 + '@example.com'
+    assert_not @user.valid?
+  end
+
+# Validate valid email address entered by user
+  test 'email validation should accept valid addresses' do
+    valid_addresses = %w[user@example.com USER@foo.com A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |address|
+      @user.email = address
+      assert @user.valid?, "#{address.inspect} should be valid"
+    end
+  end
+
+  test 'email validation should reject invalid addresses' do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.foo@bar_baz.com foo@bar+baz.com foo@bar..com]
+    invalid_addresses.each do |address|
+      @user.email = address
+      assert_not @user.valid?, "#{address.inspect} should be invalid"
+    end
   end
 
 end
