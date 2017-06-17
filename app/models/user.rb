ca102edc5e16c6b4322e4ce463b57b068bb7e428
Erik Johnson <mindovermiles262@gmail.com>
2017-06-17 12:03:37 -0700

    fix user model merge conflict

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,6 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 }, 
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
-<<<<<<< HEAD
-=======
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
->>>>>>> bd249b44ecd7a2e5130c59cd64fdedb013df1e02
 end
